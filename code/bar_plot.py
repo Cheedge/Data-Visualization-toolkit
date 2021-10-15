a7e1681f76515549bfe763e5815773256e5292ed
Cheedge <cheedgelee@gmail.com>
2021-10-15 13:32:59 +0200

    add func to extract question choices from xml file, and also add func to make user to change choice names if it too long

--- code/bar_plot.py
+++ code/bar_plot.py
@@ -6,6 +6,7 @@ import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from lime import read_lime_questionnaire_structure
 from matplotlib import ticker
 
 
@@ -19,7 +20,7 @@ def bar_plot_settings(
     legend_loc: str = None,
     legend_col: int = 2,
     save_fig: bool = False,
-    no_frame: bool = True,
+    no_frame: bool = False,
     row_sum: int = None,
 ) -> Tuple[mpl.figure.Figure, mpl.axes.Axes]:
     """
@@ -109,29 +110,38 @@ def extract_data(file: str, col_name: str) -> pd.Series:
     return a dataframe which include bar plot data.
     """
     df = pd.read_csv(file)
-    ser = df.fillna("no_answer")
+    ser = df.fillna("No Answer")
     return ser[col_name]
+
+
+def extract_question(xml_dict: dict, col_name: str) -> dict:
+    questions = xml_dict["questions"]
+    for ques in questions:
+        if ques["name"] == col_name:
+            name_dict = ques["choices"]
+    return name_dict
 
 
 def add_text_to_patch(
     ax: mpl.axes.Axes, font_size: float, values: list
 ) -> mpl.axes.Axes:
     for i, p in enumerate(ax.patches):
-        percent = f"{100 * p.get_height()/len(values):.1f}%"
+        percent = f"{100 * p.get_height()/sum(values):.1f}%"
         num_i = f"{values[i]}"
         ax.annotate(
             percent,
-            (p.get_x(), p.get_height() + 0.2),
+            (p.get_x() + p.get_width() * 0.3, p.get_height() + 0.2),
             fontsize=font_size * 1.1,
             color="dimgrey",
         )
-        ax.annotate(
-            num_i,
-            (p.get_x() + p.get_width() * 0.45, p.get_y() + 0.2),
-            fontsize=font_size * 1.1,
-            fontweight="heavy",
-            color="white",
-        )
+        if values[i] != 0:
+            ax.annotate(
+                num_i,
+                (p.get_x() + p.get_width() * 0.45, p.get_y() + 0.2),
+                fontsize=font_size * 1.1,
+                fontweight="heavy",
+                color="white",
+            )
     return ax
 
 
@@ -143,7 +153,7 @@ def fading_colors(interval: int, color_name: str) -> list:
         r, g, b, _ = mcolors.to_rgba(color_name)
     except ValueError:
         print(
-            "Wrong name, please refer to https://matplotlib.org/stable/gallery/color/named_colors.html"
+            "Laushir, Wrong name, please refer to https://matplotlib.org/stable/gallery/color/named_colors.html"
         )
         raise
     a = np.linspace(0.2, 1, interval)
@@ -151,26 +161,69 @@ def fading_colors(interval: int, color_name: str) -> list:
     return grad_colors
 
 
-def bar_plot(
+def produce_xy(
     ser: pd.Series,
+    org_name_dict: dict,
+    display_noanswer: bool,
+    replace_name_dict: dict = None,
+) -> Tuple[list]:
+    count = Counter(ser)
+    print(
+        f"Laushir, if name is tai long and you want to change, here provides the {org_name_dict.values() = },\
+        zayang."
+    )
+    if replace_name_dict:
+        name_dict = dict()
+        for key, val in org_name_dict.items():
+            if val in replace_name_dict.keys():
+                name_dict[key] = replace_name_dict[val]
+            else:
+                name_dict[key] = val
+    else:
+        name_dict = org_name_dict
+
+    # reorder dict
+    cnt = dict()
+    # for i, _ in name_dict.items():
+    #     if i in count.keys():
+    #         cnt[name_dict[i]] = count[i]
+    #     else:
+    #         cnt[name_dict[i]] = 0
+
+    cnt = {
+        name_dict[i]: count[i] if i in count.keys() else 0 for i, _ in name_dict.items()
+    }
+    cnt["No Answer"] = count["No Answer"]
+
+    if display_noanswer:
+        x = cnt.keys()
+    else:
+        cnt.pop("No Answer")
+        x = cnt.keys()
+
+    y = list(cnt.values())
+    return x, y
+
+
+def bar_plot(
+    csv_file_path: str,
+    col_name: str,
     color: str,
+    structure_dict: dict,  # read from xml
     display_noanswer: bool,
     save_fig: str,
-    f_size: tuple = (20, 6),
+    f_size: tuple = (12, 6),
     give_title: str = None,
     give_x_label: str = None,
     give_y_label: str = None,
+    replace_name_dict: dict = None,
 ) -> Tuple[mpl.figure.Figure, mpl.axes.Axes]:
     """
     plot single choice bar graph
     """
-    cnt = Counter(ser)
-    if display_noanswer:
-        x = cnt.keys()
-    else:
-        cnt.pop("no_answer")
-        x = cnt.keys()
-    y = list(cnt.values())
+    ser = extract_data(csv_file_path, col_name)
+    name_dict = extract_question(structure_dict, col_name)
+    x, y = produce_xy(ser, name_dict, display_noanswer, replace_name_dict)
     fig, ax = plt.subplots(figsize=f_size)
     ax.bar(x, y, color=fading_colors(len(y), color))
     add_text_to_patch(ax, font_size=f_size[0], values=y)
@@ -181,10 +234,20 @@ def bar_plot(
     return fig, ax
 
 
-"""
 # final version I will remove it, keep it just for testing else I have to find the test file and others.
-# if __name__=='__main__':
-#     ser = extract_data('../../../DataScience/Matplotlib/test_Oct08.csv', 'A3')
-#     bar_plot(ser, 'orang', display_noanswer=True, save_fig='./bar_img.png', give_title='title', give_y_label='y label')
-#     print(f'finished')
-# """
+if __name__ == "__main__":
+    structure_dict = read_lime_questionnaire_structure("../data_set/test_Oct.xml")
+    bar_plot(
+        csv_file_path="../../../DataScience/Matplotlib/test_Oct08.csv",
+        col_name="C2",
+        structure_dict=structure_dict,
+        color="orange",
+        display_noanswer=True,
+        save_fig="../playground/tmp/bar_img.png",
+        replace_name_dict={"I don’t want to answer this question": "No Comment"},
+        #                     'Gender diverse (Gender-fluid)':"Gender diverse",
+        #                     'Other gender representations:': 'Other'},\
+        give_title="DeGene",
+        give_y_label="y label",
+    )
+    print(f"{'finished'}")
